c2573f8ee1f6bb0ab6fc0bf5f1550cf2149429f9
Gregor <gregorlthomson@sky.com>
2019-07-24 19:43:42 +0100

    Improve functionality of cycle to avoid HTML style manipulation

--- script.js
+++ script.js
@@ -3,23 +3,31 @@ let recipeDisplay = 0;
 
 function hideAll() {
   recipeCards.forEach(element => {
-    element.style.display = "none";
+    element.classList.toggle("hidden");
+  });
+}
+
+function listAll() {
+  recipeCards.forEach(element => {
+    console.log(element.classList);
   });
 }
 
 function initialise() {
   hideAll();
-  recipeCards[recipeDisplay].style.display = "grid";
+  recipeCards[recipeDisplay].classList.toggle("hidden");
 }
 
 setInterval(function() {
   if (recipeDisplay < recipeCards.length - 1) {
     recipeDisplay++;
+    recipeCards[recipeDisplay - 1].classList.toggle("hidden");
+    recipeCards[recipeDisplay].classList.toggle("hidden");
   } else {
     recipeDisplay = 0;
+    recipeCards[recipeCards.length - 1].classList.toggle("hidden");
+    recipeCards[recipeDisplay].classList.toggle("hidden");
   }
-  hideAll();
-  recipeCards[recipeDisplay].style.display = "grid";
-}, 5000);
+}, 3000);
 
 initialise();
